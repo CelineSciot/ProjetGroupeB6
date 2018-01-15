8c5f8cdfdd96491274058b7a41f3a6615646c9e7
CelineSciot <celine.sciot@hotmail.com>
2018-01-15 12:00:32 +0100

    ajout pages HTML+ classe Action

---
+++ groupeB6/src/main/java/be/helha/groupeB6/entities/Action.java
@@ -0,0 +1,18 @@
+package be.helha.groupeB6.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Action implements Serializable{
+
+	
+	private int id;
+	
+	private Date dateA;
+	private String titre,description,typeCollecte, image;
+	
+	private double objectifFinancier;
+	
+	
+	
+}
